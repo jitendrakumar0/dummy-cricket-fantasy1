2bd028274606473486a1f4d0b8813a537859a68d
jitendrakumar0 <70063345+jitendrakumar0@users.noreply.github.com>
2023-02-03 10:49:01 +0530

    Update script_root.php

--- layouts/script_root.php
+++ layouts/script_root.php
@@ -3,9 +3,9 @@ $get_url = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 $arr = explode("?", $get_url, 2);
 $actual_link = $arr[0];
 if ($_SERVER["HTTP_HOST"] == "localhost") {
-   define('f_path', 'http://localhost/cr-sports/');
+   define('f_path', 'http://localhost/dummy-cricket-fantasy1/');
 } else if ($_SERVER['HTTP_HOST'] == '192.168.1.50') {
-   define('f_path', 'http://192.168.1.50/cr-sports/');
+   define('f_path', 'http://192.168.1.50/dummy-cricket-fantasy1/');
 } else {
-   define('f_path', 'http://143.110.244.110/cr-sports/');
+   define('f_path', 'http://143.110.244.110/dummy-cricket-fantasy1/');
 }
